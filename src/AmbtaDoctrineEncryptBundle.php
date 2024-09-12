2beb27c9870e2e3d2f4ab844e71934c2df0f35f4
Yorkie Chadwick <y.chadwick@networking.ch>
2024-09-12 15:23:54 +0200

    initial upgrade
    Signed-off-by: Yorkie Chadwick <y.chadwick@networking.ch>

--- src/AmbtaDoctrineEncryptBundle.php
+++ src/AmbtaDoctrineEncryptBundle.php
@@ -2,6 +2,7 @@
 
 namespace Ambta\DoctrineEncryptBundle;
 
+use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Compiler\PassConfig;
@@ -15,7 +16,7 @@ class AmbtaDoctrineEncryptBundle extends Bundle {
         $container->addCompilerPass(new RegisterServiceCompilerPass(), PassConfig::TYPE_AFTER_REMOVING);
     }
     
-    public function getContainerExtension()
+    public function getContainerExtension():?ExtensionInterface
     {
         return new DoctrineEncryptExtension();
     }
